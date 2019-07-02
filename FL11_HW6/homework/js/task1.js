9962496da9029fbb8de23a2076cb73f65321288b
unknown <irunaradionova@gmail.com>
2019-07-02 21:05:10 +0300

    template for first task

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,11 @@
-// Your code goes here
+let aX = 0,
+    aY = 0,
+    bX = 2,
+    bY = 2,
+    cX = 1,
+    cY = 1,
+    middlePoitX = (aX + bX)/2,
+    middlePoitY = (aY + bY)/2,
+    check = (cX == middlePoitX) && (cY = middlePoitY);
+console.log(Boolean(check));
+
