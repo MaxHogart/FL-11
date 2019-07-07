f0366c91d0f49037c51bf974c671db67b26a5222
unknown <irunaradionova@gmail.com>
2019-07-07 23:07:56 +0300

    task1 hw7

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,63 @@
-// Your code goes here
+let enteredEmail = prompt('Please enter your email', ''),
+    minSymbols = 5,
+    usersEmail = [{
+            email: 'user@gmail.com',
+            password: 'UserPass'
+        },
+        {
+            email: 'admin@gmail.com',
+            password: 'AdminPass'
+        }
+    ],
+    validEmail = false,
+    minPassword = 5,
+    rightPassword,
+    enteredPassword,
+    changePassword;
+
+for (let i = 0; i < usersEmail.length; i++) {
+    if (enteredEmail === usersEmail[i].email) {
+        validEmail = true;
+        rightPassword = usersEmail[i].password;
+    }
+}
+if (enteredEmail) {
+    if (enteredEmail.length < minSymbols) {
+        alert(`I don't know any emails having name length less than 6 symbols`);
+    } else if (validEmail) {
+        enteredPassword = prompt('Please enter your password', '');
+        if (!enteredPassword) {
+            alert('Canseled')
+            } else if(enteredPassword === rightPassword ) {                
+                changePassword = confirm('Do you want to change your password?');
+            if (changePassword) {
+                let oldPassword = prompt('Please enter your old password', '');
+                if (oldPassword === rightPassword) {
+                    let newPassword = prompt('Please enter your new password', '');
+                    if (newPassword.length < minPassword) {
+                        alert('It’s too short password. Sorry.')
+                    } else {
+                        let secondNewPassword = prompt('Please enter your new password again', '');
+                        if (secondNewPassword === newPassword) {
+                            alert('You have successfully changed your password.');
+                        } else {
+                            alert('You wrote the wrong password.');
+                        }
+                    }
+                } else if (oldPassword) {
+                    alert('Wrong password');
+                } else {
+                    alert('Canceled');
+                }
+            } else {
+                alert('You have failed the change.')
+            }
+        } else {
+            alert('Wrong password')
+        }
+    } else if (enteredEmail.length > minSymbols) {
+        alert('I don’t know you')
+    }
+} else {
+    alert('Canceled');
+}
