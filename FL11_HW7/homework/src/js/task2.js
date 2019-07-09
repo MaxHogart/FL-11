0a7aa8b452df9a5f901484b22988a0589605ce28
unknown <irunaradionova@gmail.com>
2019-07-09 12:47:43 +0300

    final HW 7

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -2,42 +2,48 @@ let userAgree = confirm('Do you want to play a game?'),
     minNumber = 0,
     maxNumber = 8,
     winNumber = 0,
-    // userNumber,
-    totalPrize = 0,
+    userNumber = 0,
+    totalPrize = 0,    
     startPossiblePrize = 100,
     attemptsNumber = 3,
-    userPrize = 0,
     declineCoefPrize = 2,
     ariseCoefNumb = 4,
     ariseCoefPrize = 2;
+
+const firstPrize = 100,
+      firstMaxNumber = 8;  
 
 if (userAgree) {
     while (userAgree) {
         winNumber = Math.floor(Math.random() * (maxNumber + 1));
         console.log(winNumber);            
-        let possiblePrize = startPossiblePrize,
-            userNumber = 0;
+        let possiblePrize = startPossiblePrize;
         for (let i=attemptsNumber; i>0; i--) {
             let textForGame = `Choose a roulette pocket number from ${minNumber} to ${maxNumber}
 Attempts left: ${i}
 Total prize: ${totalPrize}$
 Possible prize on current attempt: ${possiblePrize}$`;
             userNumber = +prompt(textForGame);
-            console.log(userNumber);
             if (userNumber === winNumber) {
-                userPrize += possiblePrize;
-                let textForWin = `Congratulation, you won! Your prize is: ${userPrize}$. Do you want to continue?`;
+                totalPrize += possiblePrize;
+                let textForWin = `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`;
                 userAgree = confirm(textForWin);
                 break;
             }
             possiblePrize /= declineCoefPrize;
         }
-        console.log(userNumber);
-        console.log(possiblePrize);
-        if (userNumber !== winNumber) {
-            let textForLose = `Thank you for your participation. Your prize is: ${userPrize}$`;
+        if (userNumber !== winNumber || !userAgree) {
+            if (userNumber !== winNumber) {
+                totalPrize = 0;
+            }            
+            let textForLose = `Thank you for your participation. Your prize is: ${totalPrize}$`;
             alert(textForLose);
             userAgree = confirm(`Do you want to play again?`);
+            if (userAgree) {
+                totalPrize = 0;
+            }
+            startPossiblePrize = firstPrize;
+            maxNumber = firstMaxNumber;
         } else {
             maxNumber += ariseCoefNumb;
             startPossiblePrize *= ariseCoefPrize;
