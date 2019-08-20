943014e4e3737faefbcef588379d37ba9f7ec77f
Iryna Radionova <irunaradionova@gmail.com>
2019-08-20 23:47:03 +0300

    Update task1

--- FL_11_16_homework_inheritance/homework/js/task1.js
+++ FL_11_16_homework_inheritance/homework/js/task1.js
@@ -2,14 +2,10 @@ function assign () {
     let obj = arguments[0];
     for (let i=1; arguments.length>i; i++) {
         for (let key in arguments[i]) {
-            obj[key] = arguments[i][key];
+            if (Object.prototype.hasOwnProperty.call(arguments[i], key)) {
+                obj[key] = arguments[i][key];
+            }
         }
     }
     return obj;
 }
-
-const defaults = {a:123, b:777};
-const options = {a:456, x: 44};
-const configs = assign({}, defaults, options);
-
-console.log(configs);
